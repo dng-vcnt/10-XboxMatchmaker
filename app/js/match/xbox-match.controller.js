224cb813e8f23e80b83a637fc5cb4ce7fbcee782
Ailuridaes <ailuridaes@gmail.com>
2016-08-19 16:44:21 -0700

    randomizes which friends to get friends from

--- app/js/match/xbox-match.controller.js
+++ app/js/match/xbox-match.controller.js
@@ -23,7 +23,7 @@
             xboxMatchFactory.getMatches(vm.gamertag).then (
                 function(data){
                     vm.xboxTeam = data;
-                    console.log(vm.xboxTeam);
+                    // console.log(vm.xboxTeam);
                 },
                 function(error){
                     console.log(error);
